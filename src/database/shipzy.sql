60106754409e0a55510555799d8562bf95e2e162
Fush <ahron.badili@bisu.edu.ph>
2025-05-07 20:41:24 +0800

    Add files via upload

--- src/database/shipzy.sql
+++ src/database/shipzy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2025 at 02:24 PM
+-- Generation Time: May 07, 2025 at 02:40 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
